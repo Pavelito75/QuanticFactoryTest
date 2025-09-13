1ea9ef7ec95365a62098e2c046bfdce77101c766
Pavelito75 <pavel.gatti@epitech.eu>
2025-09-13 16:49:29 +0200

    [FEAT] : return data in html table

--- quantic/src/app/page.js
+++ quantic/src/app/page.js
@@ -1,42 +1,71 @@
 import styles from "./page.module.css";
 
 export default async function Home() {
-  let activities = await fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/ilots-de-fraicheur-equipements-activites/records?')
+  let activities = await fetch('https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/ilots-de-fraicheur-equipements-activites/records?order_by=arrondissement&limit=20')
   let jsonAct = await activities.json()
   let postsAct = jsonAct.results
 
-  let fresh = await fetch('https://parisdata.opendatasoft.com/api/explore/v2.1/catalog/datasets/ilots-de-fraicheur-espaces-verts-frais/records?')
+  let fresh = await fetch('https://parisdata.opendatasoft.com/api/explore/v2.1/catalog/datasets/ilots-de-fraicheur-espaces-verts-frais/records?order_by=arrondissement&limit=20')
   let jsonFresh = await fresh.json()
   let postsFresh = jsonFresh.results
 
-  let drink = await fetch('https://parisdata.opendatasoft.com//api/explore/v2.1/catalog/datasets/fontaines-a-boire/records?')
+  let drink = await fetch('https://parisdata.opendatasoft.com//api/explore/v2.1/catalog/datasets/fontaines-a-boire/records?limit=20')
   let jsonDrink = await drink.json()
   let postsDrink = jsonDrink.results
 
 
-
   return (
-    <div>
-      <h1>Activites :</h1>
-      <ul>
+    <div id="true">
+      {/* <h1>Activites :</h1> */}
+      <h1>Tableau :</h1>
+      <table>
+        <tr>
+          <td>LIEUX</td>
+          <td>ACTIVITES</td>
+          <td>PAYANT</td>
+          <td>ADRESSE</td>
+          <td>ARRONDISSEMENT</td>
+          <td>HORAIRES D'OUVERTURES</td>
+        </tr>
         {postsAct.map((post) => (
-          <li key={post.identifiant}>{post.nom}</li>
+          <tr key={post.identifiant} className="activity">
+            <td>{post.nom}</td>
+            <td>{post.type}</td>
+            <td>{post.payant}</td>
+            <td>{post.adresse}</td>
+            <td>{post.arrondissement}</td>
+            <td>{post.horaires_periode || "On sait pas"}</td>
+          </tr>
         ))}
-      </ul>
-      <hr></hr>
-      <h1>Espaces verts :</h1>
-      <ul>
+      {/* <hr></hr>
+      <h1>Espaces verts :</h1> */}
         {postsFresh.map((postFresh) => (
-          <li key={postFresh.identifiant}>{postFresh.nom}</li>
+          <tr key={postFresh.identifiant} className="green-space">
+            <td>{postFresh.nom}</td>
+            <td>{postFresh.type}</td>
+            <td>{postFresh.payant}</td>
+            <td>{postFresh.adresse}</td>
+            <td>{postFresh.arrondissement}</td>
+            <td>{postFresh.horaires_periode || "On sait pas"}</td>
+          </tr>
         ))}
-      </ul>
-      <hr></hr>
-      <h1>Fontaines :</h1>
-      <ul>
+      </table>
+      <table>
+        <tr>
+          <td>N°</td>
+          <td>VOIE</td>
+          <td>COMMUNE</td>
+        </tr>
+      {/* <hr></hr>
+      <h1>Fontaines :</h1> */}
         {postsDrink.map((postDrink) => (
-          <li key={postDrink.gid}>{postDrink.no_voirie_pair || postDrink.no_voirie_impair} {postDrink.voie} / {postDrink.commune}</li>
+          <tr key={postDrink.gid} className="fountain">
+            <td>{postDrink.no_voirie_pair || postDrink.no_voirie_impair}</td>
+            <td>{postDrink.voie}</td>
+            <td>{postDrink.commune}</td>
+          </tr>
         ))}
-      </ul>
+      </table>
     </div>
   )
 }
